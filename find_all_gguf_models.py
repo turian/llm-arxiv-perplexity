c13477283f4fff976cf5853f6b4eda9081c32dab
Joseph Turian <>
2024-05-01 20:23:43 -0400

    find_all_gguf_models.py

--- find_all_gguf_models.py
+++ find_all_gguf_models.py
@@ -1,5 +1,6 @@
 import glob
 import re
+import random
 
 BASE_DIR = "/Users/joseph/dev/HuggingFaceModelDownloader/"
 
@@ -7,12 +8,15 @@ general_pattern = re.compile(r'[0-9]-of-[0-9]')
 exclusion_pattern = re.compile(r'00001-of-')
 
 def find_all_gguf_models():
-    gguf_models = glob.glob(BASE_DIR + "**/*gguf", recursive=True)
+    gguf_models = list(glob.glob(BASE_DIR + "**/*gguf", recursive=True))
+    random.seed(0)
+    random.shuffle(gguf_models)
     for model in gguf_models:
         if general_pattern.search(model) and not exclusion_pattern.search(model):
              continue
-        print(model)
+        yield model
 
 
 if __name__ == "__main__":
-    find_all_gguf_models()
+    for m in find_all_gguf_models():
+        print(m)
